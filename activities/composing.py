2d77633cdf3ef9a653111e08ea6dfda2077b5201
Mark Hopkins <hopkinsm@reed.edu>
2021-02-10 17:11:11 -0800

    added solution to composing

---
+++ activities/composing.py
@@ -0,0 +1,11 @@
+def compose(f, g):
+    return lambda x: f(g(x))
+
+def compose2(f, g):
+    func = lambda x: f(g(x))
+    return func
+
+def compose3(f, g):
+    def func(x):
+        return f(g(x))
+    return func
