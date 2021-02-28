ee0573ea0f785ba00e4ba44d8f4fb3d834632a74
Mark Hopkins <hopkinsm@reed.edu>
2021-02-28 14:15:46 -0800

    Added Activity: Market Research

---
+++ activities/marketresearch.py
@@ -0,0 +1,13 @@
+populations = {('sesamopolis', 'sesame'): 4200000,
+               ('sesameville', 'sesame'): 2700,
+               ('springfield', 'sesame'): 51000,
+               ('silopomases', 'emases'): 2400000,
+               ('ellivemases', 'emases'): 4200000,
+               ('kermiton', 'muppet empire'): 57000,
+               ('kermitberg', 'muppet empire'): 125000,
+               ('kermitshire', 'muppet empire'): 1200,
+               ('springfield', 'oregon'): 62000}
+
+
+def focus_on(state, population_dict):
+    """ Fill in your answer to Activity: Market Research. """
