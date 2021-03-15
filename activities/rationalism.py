37d740555b513c43a8aac9987972669c7398db1c
Mark Hopkins <hopkinsm@reed.edu>
2021-03-14 23:30:19 -0700

    Added starter code for Rationalism.

---
+++ activities/rationalism.py
@@ -0,0 +1,13 @@
+class Fraction:
+
+    def initialize(self, n, d):
+        """ Fiil this in! """
+
+    def numerator(self):
+        """ Fiil this in! """
+
+    def denominator(self):
+        """ Fiil this in! """
+
+    def multiply_by(self, other_frac):
+        """ Fiil this in! """
